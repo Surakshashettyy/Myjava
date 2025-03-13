8acfd63e8a758cc7f3b6be4c2547b1b5717ccb5b
Suraksha Shetty <surakshashetty1974@gmail.com>
2025-03-13 22:49:49 +0530

    Hello world update
    my first java program

---
+++ Firstprogram.java
@@ -0,0 +1,9 @@
+//public - public property can be used by anyone 
+public class Firstprogram
+{
+    public static void main(String args[])
+    {
+        //Print
+        System.out.println("Hello World!");
+    }
+}
