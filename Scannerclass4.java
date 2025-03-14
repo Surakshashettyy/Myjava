7ea2cec3da8616e8f1e744eb88b2b594624bf43f
Suraksha Shetty <surakshashetty1974@gmail.com>
2025-03-14 17:09:27 +0530

    Update Scannerclass4.java

--- Scannerclass4.java
+++ Scannerclass4.java
@@ -3,16 +3,19 @@ public class Scannerclass4 {
     public static void main(String args[])
     {
         Scanner obj=new Scanner(System.in); //System.in used for taking input from the console
+
         int a = obj.nextInt();
         float b = obj.nextFloat();
         double c = obj.nextDouble();
         String w = obj.next();     //next() is used for one word input
         String s = obj.nextLine(); //nextLine() is used for multiple word input
+        char d = obj.next().charAt(0); //charAt(0) is used to take the first character of the input
 
         System.out.println("The value of a is " +a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(w);
         System.out.println(s);
+        System.out.println(d);
     }
 }
