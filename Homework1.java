82ad6a14f6cd8af5cccfd460ec7761fd43731953
Suraksha Shetty <surakshashetty1974@gmail.com>
2025-03-14 19:50:22 +0530

    homework update

---
+++ Homework1.java
@@ -0,0 +1,14 @@
+//Enter two numbers and perform all arithmetic operations
+
+import java.util.Scanner;
+public class Homework1{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        char a=obj.next().charAt(0);
+        int b=obj.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
