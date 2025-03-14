c1e7bf002f5c372b718c9693a18767a5c8ae7fed
Suraksha Shetty <surakshashetty1974@gmail.com>
2025-03-14 20:02:25 +0530

    Create LanguageQ1.java

---
+++ LanguageQ1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class LanguageQ1 {
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        char a=obj.next().charAt(0);
+        int b=obj.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+    
