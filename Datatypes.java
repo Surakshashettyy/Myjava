c77a0f31a4260024ea9f86bb0cadba2a48222449
Suraksha Shetty <surakshashetty1974@gmail.com>
2025-03-14 12:01:08 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,18 @@
+public class Datatypes {
+
+    public class Main{
+        public static void main(String args[])
+        {
+            int a=2;
+            double b=3.45; 
+            char c='S'; //Single quote-single letter
+            String d="uraksha is the best"; //Double quote-multiple letters
+            
+            // If we want to join english sentence with variable we use + operator
+            System.out.println("The value of a is " +a);
+            System.out.println("Double can hold multiple decimal points " +b+ " for example");
+            System.out.print(c);
+            System.out.println(d);
+        }
+    }
+}
