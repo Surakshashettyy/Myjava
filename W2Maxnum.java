407626874bdc626feb036b1bd255fdb5e9b0c6b8
Suraksha Shetty <surakshashetty1974@gmail.com>
2025-03-15 13:33:21 +0530

    Week2
    update

---
+++ W2Maxnum.java
@@ -0,0 +1,16 @@
+public class W2Maxnum{
+    public static void main(String args[]){
+        int a=6;
+        int b=6;
+            
+        if(a>b){
+            System.out.println("Max number: " +a);
+        }
+        else if(a<b){
+            System.out.println("Max number: " +b);
+        }
+        else{
+            System.out.println("Equal");
+        }
+    }
+}
